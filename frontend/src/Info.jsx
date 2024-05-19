ada04beeef74e69c4f678d802d38e46ea43de8f3
Andrew Haas <ahaas@micds.org>
2024-05-19 00:24:27 +0000

    restructured database in Info.jsx

--- frontend/src/Info.jsx
+++ frontend/src/Info.jsx
@@ -1,12 +1,61 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import tennisService from "./services/tennisService";
+import { json } from "react-router-dom";
 
-function Info() {
+const Info = (props) => {
+  const [rawDatabase, setRawDatabase] = useState({});
+  const [database, setDatabase] = useState({});
+  const [times, setTimes] = useState([]);
+  const [names, setNames] = useState({});
+
+  useEffect(() => {
+    tennisService.getAll().then((newDatabase) => {
+      setRawDatabase(newDatabase);
+      console.log("got database", JSON.stringify(newDatabase));
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!rawDatabase || Object.keys(rawDatabase).length === 0) return;
+
+    const { times, names, responses } = rawDatabase;
+    var newDatabase = {};
+
+    Object.keys(responses).forEach((personID) => {
+      Object.entries(responses[personID]).forEach(([time, answer]) => {
+        if (!newDatabase[time]) {
+          newDatabase[time] = [];
+        }
+        newDatabase[time].push({
+          name: names[personID].name,
+          id: personID,
+          answer: answer,
+        });
+      });
+    });
+
+    console.log("database:", newDatabase);
+    console.log("times:", times);
+    console.log("names:", names);
+    setTimes(rawDatabase["times"]);
+    setNames(rawDatabase["names"]);
+    setDatabase(newDatabase);
+  }, [rawDatabase]);
+
+  if (Object.keys(rawDatabase).length === 0) {
+    return (
+      <div>
+        <h1>Please wait for server connection</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>Info</h2>
-      <p>Info content to be added here.</p>
+      <div>{}</div>
     </div>
   );
-}
+};
 
 export default Info;
